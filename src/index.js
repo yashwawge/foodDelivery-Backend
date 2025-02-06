3d343de3c0d90bec5822c791cac713d87ddfddf9
Yash Wawge <yashwawge01@gmail.com>
2025-02-07 00:50:50 +0530

    Image Upload setup done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const User = require('./schema/userSchema');
 const userRouter = require('./routes/userRoutes');
 const cartRouter = require('./routes/cartRoutes');
 const authRouter = require('./routes/authRoute');
+const uploader = require('./middlewares/multerMiddleware');
+const cloudinary = require('./config/cloudinaryConfig');
+const fs = require('fs/promises');
 
 const app = express();
 
@@ -20,6 +23,15 @@ app.use(express.urlencoded({extended : true}));
 app.use('/users',userRouter);  //connects the router to the server
 app.use('/carts',cartRouter);
 app.use('/auth',authRouter);
+
+app.post('/photo',uploader.single('incomming file') ,async (req,res)=>{
+
+    console.log(req.file);
+    const result = await cloudinary.uploader.upload(req.file.path);
+    console.log("result from cloudinary: ",result);
+    await fs.unlink(req.file.path);
+    return res.json({message: 'ok'});
+})
 
 app.listen(serverConfig.PORT, async ()=>{
     
