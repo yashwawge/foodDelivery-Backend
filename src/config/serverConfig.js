c8fb73964c82d81aa2990ded3d9055e0e529a6da
Yash Wawge <yashwawge01@gmail.com>
2025-02-07 00:05:47 +0530

    Added cloudinary config:

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -8,5 +8,8 @@ module.exports = {
     PORT : process.env.PORT ,
     DB_URL : process.env.DB_URL,
     JWT_SECRET : process.env.JWT_SECRET,
-    JWT_EXPIRY : process.env.JWT_EXPIRY
+    JWT_EXPIRY : process.env.JWT_EXPIRY,
+    CLOUDINARY_CLOUD_NAME : process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY : process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET : process.env.CLOUDINARY_API_SECRET
 }
