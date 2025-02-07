0dad285cacc285912cfd86503b35fbdd7effe55d
Yash Wawge <yashwawge01@gmail.com>
2025-02-07 23:10:10 +0530

    Updated git ignore

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -15,7 +15,7 @@ async function addProduct(req,res){
         const product  = await createProduct({
             productName : req.body.productName,
             description : req.body.description,
-            imagePath : req.file.path,
+            imagePath : req.file?.path,
             price : req.body.price,
             category : req.body.category,
             inStock : req.body.inStock
